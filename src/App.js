ee0cd021700877c836e49703426bc7c217db09da
Henry Kam <henry.j.kam@gmail.com>
2021-12-15 21:55:52 -0800

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Error from './pages/Error';
+import Home from './pages/Home';
+import "./styles/style.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/career-portfolio" element={<Home />} />
+          {/* Add Other Paths Here */}
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Nav.js
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import hamburger from "../assets/hamburger.png";
+
+
+function ButtonAppBar() {
+    const [open, setOpen] = React.useState(false);
+    const toggleDrawer = () => {
+        setOpen(!open);
+        console.log("clicked");
+    };
+    return (
+        <nav id="navbar">
+            <div id = "navbar-logo">Some Logo Here</div>
+                <img src={hamburger} id = "hamburger-icon" onClick={toggleDrawer}></img>
+        </nav>
+    );
+}
+
+export default function Nav() {
+    return <ButtonAppBar />;
+}
